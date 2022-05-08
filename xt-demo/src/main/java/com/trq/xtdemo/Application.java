c3b8a67e72779c742918ecc36549380eda17d49a
tangruiqiao18 <1084440483@qq.com>
2022-05-08 12:57:06 +0800

    update

--- xt-demo/src/main/java/com/trq/xtdemo/Application.java
+++ xt-demo/src/main/java/com/trq/xtdemo/Application.java
@@ -3,8 +3,10 @@ package com.trq.xtdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+//@ComponentScan(basePackages = "com.trq.xtdemo")
 public class Application {
 
     public static void main(String[] args) {
